e3d1f1726faa8dd580743e46f8ce7d7b5e4544ec
Zageron <hello@adambryant.ca>
2020-11-21 18:02:30 -0800

    Render

--- www/src/index.ts
+++ www/src/index.ts
@@ -4,6 +4,7 @@ import { Render } from "./render";
 Render();
 
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+const context: WebGL2RenderingContext = canvas.getContext("webgl2") as WebGL2RenderingContext;
 
-ffrust.start_game(canvas, context)
+//ffrust.start_game(canvas, context)
+ffrust.webgl2_sample(canvas, context);
