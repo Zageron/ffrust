abba8d4b681e1269794dcb48ef97e104d37f6967
Zageron <hello@adambryant.ca>
2020-11-30 17:05:04 -0800

    Clean up npm stuff
    Not too important at the moment.

--- www/src/index.ts
+++ www/src/index.ts
@@ -5,6 +5,7 @@ Render();
 
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const context: WebGL2RenderingContext = canvas.getContext("webgl2") as WebGL2RenderingContext;
+//const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 //ffrust.start_game(canvas, context)
 ffrust.webgl2_sample(canvas, context);
